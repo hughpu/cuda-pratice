0108b2d6ee1a2d3ab373ce8789d8e78aeec64ffa
hughpu <hughpu@hotmai.com>
2025-01-24 15:02:06 +0800

    feat: add sgemm implementation without share mem bank conflic solution

--- cudapractice/src/main.cu
+++ cudapractice/src/main.cu
@@ -13,6 +13,9 @@
   "USAGE:\n    ./main <demo_name>\n    demo_name: hello_world, reduce_sum, " \
   "transpose, gemm"
 
+#define OFFSET(row, col, stride) ((row) * (stride) + (col))
+#define FETCH_FLOAT4(element) (reinterpret_cast<float4 *>(&(element))[0])
+
 template <class T>
 class CudaAllocator {
  public:
@@ -221,6 +224,220 @@ void TransPose() {
   }
 
   printf("\n\nAll corrected!");
+}
+
+template <const int TBlockSizeM, const int TBlockSizeN, const int TBlockSizeK,
+          const int TThreadSizeM, const int TThreadSizeN>
+__global__ void GemmKernel(const float *__restrict__ A,
+                           const float *__restrict__ B, float *__restrict__ C,
+                           const int M, const int N, const int K) {
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+
+  constexpr int kNumThreadsInX = TBlockSizeM / TThreadSizeM;
+  constexpr int kNumThreadsInY = TBlockSizeN / TThreadSizeN;
+  constexpr int kNumThreads = kNumThreadsInX * kNumThreadsInY;
+
+  int tid = ty * kNumThreadsInX + tx;
+
+  __shared__ float a_share[2][TBlockSizeK][TBlockSizeM];
+  __shared__ float b_share[2][TBlockSizeK][TBlockSizeN];
+
+  float accum[TThreadSizeM][TThreadSizeN] = {0.0f};
+  float reg_a[2][TThreadSizeM];
+  float reg_b[2][TThreadSizeN];
+
+  constexpr int kFloatsPerCopy = 4;
+  constexpr int a_share_copy_threads_per_row = TBlockSizeK / kFloatsPerCopy;
+  constexpr int a_share_copy_row_stride =
+      kNumThreads / a_share_copy_threads_per_row;
+  constexpr int b_share_copy_threads_per_row = TBlockSizeN / kFloatsPerCopy;
+  constexpr int b_share_copy_row_stride =
+      kNumThreads / b_share_copy_threads_per_row;
+  float4 ldg_a[TBlockSizeM / a_share_copy_row_stride * kFloatsPerCopy];
+
+  int a_share_copy_start_row = tid / a_share_copy_threads_per_row;
+  int a_share_copy_col = tid % a_share_copy_threads_per_row * kFloatsPerCopy;
+  int b_share_copy_start_row = tid / b_share_copy_threads_per_row;
+  int b_share_copy_col = tid % b_share_copy_threads_per_row * kFloatsPerCopy;
+
+#pragma unroll
+  for (int i = 0; i < TBlockSizeM; i += a_share_copy_row_stride) {
+    int ldg_index = i / a_share_copy_row_stride * kFloatsPerCopy;
+    FETCH_FLOAT4(ldg_a[ldg_index]) = FETCH_FLOAT4(A[OFFSET(
+        i + a_share_copy_start_row + by * TBlockSizeM, a_share_copy_col, K)]);
+    a_share[0][a_share_copy_col][a_share_copy_start_row + i] = ldg_a[ldg_index];
+    a_share[0][a_share_copy_col + 1][a_share_copy_start_row + i] =
+        ldg_a[ldg_index + 1];
+    a_share[0][a_share_copy_col + 2][a_share_copy_start_row + i] =
+        ldg_a[ldg_index + 2];
+    a_share[0][a_share_copy_col + 3][a_share_copy_start_row + i] =
+        ldg_a[ldg_index + 3];
+  }
+
+#pragma unroll
+  for (int i = b_share_copy_start_row; i < TBlockSizeK;
+       i += b_share_copy_row_stride) {
+    FETCH_FLOAT4(b_share[0][i][b_share_copy_col]) =
+        FETCH_FLOAT4(B[OFFSET(i, b_share_copy_col + bx * TBlockSizeN, N)]);
+  }
+
+  __syncthreads();
+
+#pragma unroll
+  for (int y = 0; y < TThreadSizeM; y += kFloatsPerCopy) {
+    FETCH_FLOAT4(reg_a[0][x]) =
+        FETCH_FLOAT4(a_share[0][0][ty * TBlockSizeM + y]);
+  }
+
+#pragma unroll
+  for (int x = 0; x < TThreadSizeN; x += kFloatsPerCopy) {
+    FETCH_FLOAT4(reg_b[0][i]) =
+        FETCH_FLOAT4(b_share[0][0][tx * TBlockSizeN + x]);
+  }
+
+  // outer iteration with blockK stride
+  int write_stage_idx = 1;
+  int tile_k_idx = 0;
+  do {
+    tile_k_idx += TBlockSizeK;
+    int load_stage_idx = write_stage_idx ^ 1;
+
+    if (tile_k_idx < K) {
+#pragma unroll
+      for (int i = 0; i < TBlockSizeM; i += a_share_copy_row_stride) {
+        int ldg_index = i / a_share_copy_row_stride * kFloatsPerCopy;
+        FETCH_FLOAT4(ldg_a[ldg_index]) =
+            FETCH_FLOAT4(A[OFFSET(i + a_share_copy_start_row + by * TBlockSizeM,
+                                  a_share_copy_col + tile_k_idx, K)]);
+      }
+    }
+
+// inner iteration with single step stride on k
+#pragma unroll
+    for (int j = 0; j < TBlockSizeK - 1; ++j) {
+#pragma unroll
+      for (int y = 0; y < TThreadSizeM; ++y) {
+        reg_a[(j + 1) % 2][y] =
+            a_share[load_stage_idx][j + 1][ty * TBlockSizeM + y];
+      }
+
+#pragma unroll
+      for (int x = 0; x < TThreadSizeN; ++x) {
+        reg_b[(j + 1) % 2][x] =
+            b_share[load_stage_idx][j + 1][tx * TBlockSizeN + x];
+      }
+
+#pragma unroll
+      for (int y = 0; y < TThreadSizeM; ++y) {
+#pragma unroll
+        for (int x = 0; x < TThreadSizeN; ++x) {
+          accum[y][x] += reg_a[j % 2][y] * reg_b[j % 2][x];
+        }
+      }
+    }
+
+    if (tile_k_idx < K) {
+#pragma unroll
+      for (int i = 0; i < TBlockSizeM; i += a_share_copy_row_stride) {
+        int ldg_index = i / a_share_copy_row_stride * kFloatsPerCopy;
+        a_share[write_stage_idx][a_share_copy_col][a_share_copy_start_row + i] =
+            ldg_a[ldg_index];
+        a_share[write_stage_idx][a_share_copy_col + 1]
+               [a_share_copy_start_row + i] = ldg_a[ldg_index + 1];
+        a_share[write_stage_idx][a_share_copy_col + 2]
+               [a_share_copy_start_row + i] = ldg_a[ldg_index + 2];
+        a_share[write_stage_idx][a_share_copy_col + 3]
+               [a_share_copy_start_row + i] = ldg_a[ldg_index + 3];
+      }
+
+#pragma unroll
+      for (int i = b_share_copy_start_row; i < TBlockSizeK;
+           i += b_share_copy_row_stride) {
+        FETCH_FLOAT4(
+            b_share[write_stage_idx][i][b_share_copy_col] =
+                FETCH_FLOAT4(B[OFFSET(i + tile_k_idx,
+                                      b_share_copy_col + bx * TBlockSizeN, N)]))
+      }
+    }
+
+    __syncthreads();
+
+    if (tile_k_idx < K) {
+#pragma unroll
+      for (int y = 0; y < TThreadSizeM; y += kFloatsPerCopy) {
+        FETCH_FLOAT4(reg_a[0][x]) =
+            FETCH_FLOAT4(a_share[write_stage_idx][0][ty * TBlockSizeM + y]);
+      }
+
+#pragma unroll
+      for (int x = 0; x < TThreadSizeN; x += kFloatsPerCopy) {
+        FETCH_FLOAT4(reg_b[0][i]) =
+            FETCH_FLOAT4(b_share[write_stage_idx][0][tx * TBlockSizeN + x]);
+      }
+    }
+
+#pragma unroll
+    for (int y = 0; y < TThreadSizeM; ++y) {
+#pragma unroll
+      for (int x = 0; x < TThreadSizeN; ++x) {
+        accum[y][x] +=
+            reg_a[(TBlockSizeK - 1) % 2][y] * reg_b[(TBlockSizeK - 1) % 2][x];
+      }
+    }
+
+    write_stage_idx ^= 1;
+  } while (tile_k_idx < K);
+
+#pragma unroll
+  for (int y = 0; y < TThreadSizeM; ++y) {
+#pragma unroll
+    for (int x = 0; x < TThreadSizeN; x += kFloatsPerCopy) {
+      FETCH_FLOAT4(C[OFFSET(TBlockSizeM * by + TThreadSizeM * ty + y,
+                            TBlockSizeN * bx + TThreadSizeN * tx + x, N)]) =
+          FETCH_FLOAT4(accum[y][x]);
+    }
+  }
+}
+
+void Gemm() {
+  const int M = 1 << 16;
+  const int N = 1 << 14;
+  const int K = 1 << 8;
+  thrust::universal_vector<float> A(M * K);
+  thrust::universal_vector<float> B(K * N);
+  thrust::universal_vector<float> C(M * N);
+
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < K; ++j) {
+      A[i * K + j] =
+          ((std::rand() & ((1 << 6) - 1)) - (1 << 5)) * 1.f / (1 << 6);
+    }
+  }
+
+  for (int i = 0; i < K; ++i) {
+    for (int j = 0; j < N; ++j) {
+      B[i * N + j] =
+          ((std::rand() & ((1 << 6) - 1)) - (1 << 5)) * 1.f / (1 << 6);
+    }
+  }
+
+  GemmKernel<128, 128, 16, 8, 8><<<M / 128, N / 128, 1>>>(
+      thrust::raw_pointer_cast(A), thrust::raw_pointer_cast(B),
+      thrust::raw_pointer_cast(C), M, N, K);
+
+  checkCudaErrors(cudaDeviceSynchronize());
+
+  float acc = 0;
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < N; ++j) {
+      acc += C[i * N + j];
+    }
+  }
+
+  fprintf(stderr, "got c sum as %f", acc);
 }
 
 enum Demo {
